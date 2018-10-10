6eea83ec93a83fc80bab2ac9b6a65e370cf8e5bf
anaivanovska <annaivanovskaa@gmail.com>
2018-10-10 15:34:08 +0200

    Inital commit

---
+++ src/main/java/mk/com/ukim/finki/rent_advertisement/RentAdvertisementApplication.java
@@ -0,0 +1,12 @@
+package mk.com.ukim.finki.rent_advertisement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentAdvertisementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentAdvertisementApplication.class, args);
+	}
+}
---
+++ src/test/java/mk/com/ukim/finki/rent_advertisement/RentAdvertisementApplicationTests.java
@@ -0,0 +1,16 @@
+package mk.com.ukim.finki.rent_advertisement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentAdvertisementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
